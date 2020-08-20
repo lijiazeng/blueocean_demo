201eb1914f4abd57a4da4655e87656114f36acd6
lijiazeng <lijiazeng@outlook.com>
2020-08-20 14:55:53 +0800

    first commit

---
+++ src/main/java/com/example/blueocean/BlueoceanApplication.java
@@ -0,0 +1,13 @@
+package com.example.blueocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueoceanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueoceanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blueocean/BlueoceanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blueocean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueoceanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
